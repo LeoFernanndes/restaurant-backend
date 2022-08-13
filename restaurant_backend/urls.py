31508ac1a84a48a43622b8ef411acd1f6e16d514
LeoFernanndes <balbino.leonel@gmail.com>
2022-08-13 19:23:03 -0300

    adds swagger config

--- restaurant_backend/urls.py
+++ restaurant_backend/urls.py
@@ -15,9 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Restaurant API",
+        default_version='v1',
+        description="Api for restaurant management.",
+        contact=openapi.Contact(email="contato@leobalbino.com"),
+    ),
+    public=True,
+    permission_classes=[AllowAny]
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('people/', include('people.routes'), name='people')
+    path('people/', include('people.routes'), name='people'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
